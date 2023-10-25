ca6aabc57de7ef8d2b5a1f7f291dcf4dfc75de76
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-25 17:01:39 +0200

    Challenge completed

--- pages/volumes/index.js
+++ pages/volumes/index.js
@@ -1,26 +1,31 @@
 import Link from "next/link";
 import { introduction } from "../../lib/data";
+import { volumes } from "../../lib/data.js";
+import { useRouter } from "next/router";
 
 export default function Volumes() {
+  const router = useRouter();
+
+  function getRandomElement() {
+    return volumes[Math.floor(Math.random() * volumes.length)];
+  }
+
+  const handleRandomPush = () => {
+    router.push(`/volumes/${getRandomElement().slug}`);
+  };
+
   return (
     <>
       <h1>The Lord of the Rings</h1>
       <p>{introduction}</p>
       <ul>
-        <li>
-          <Link href="/volumes/the-fellowship-of-the-ring">
-            The Fellowship of the Ring
-          </Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-two-towers">The Two Towers</Link>
-        </li>
-        <li>
-          <Link href="/volumes/the-return-of-the-king">
-            The Return of the King
-          </Link>
-        </li>
+        {volumes.map((volume) => (
+          <li key={volume.title}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
       </ul>
+      <button onClick={() => handleRandomPush()}>RANDOM</button>
     </>
   );
 }
